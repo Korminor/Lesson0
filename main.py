396da7f36a7d7aeef8e774afad3916506d39ff38
Korminor <BakulinEU@gmail.com>
2024-09-07 22:29:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# "1st program"
+
+print((9**0.5)*5)
+
+# "2nd program"
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# "3rd program"
+
+a = 2 * 2 + 2
+print(a)
+b = 2 * (2 + 2)
+print(b)
+c = a == b
+print(c)
+
+#"4th program"
+
+s = '123.456'
+f = float(s)
+f1 = f*10
+i = int(f1)
+print(i%10)
